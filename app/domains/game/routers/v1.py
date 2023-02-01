21657e815328beb80d063781740f3c17bcc4da40
Garuso Cruz <garusotec@gmail.com>
2023-01-31 22:09:48 -0300

    Create new routers

--- app/domains/game/routers/v1.py
+++ app/domains/game/routers/v1.py
@@ -2,13 +2,17 @@
 game module having all routes from v1
 """
 
-from fastapi import APIRouter, Response, status
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse
+
+from app.domains.game.engine import Engine
+from app.domains.game.models import Game
 
 router = APIRouter()
 
 
 @router.get("/")
-async def index(res: Response):
+async def index():
     """fetch v1 index game endpoint
 
     Args:
@@ -17,6 +21,38 @@ async def index(res: Response):
     Returns:
         dict: {}
     """
-    res.status_code = status.HTTP_200_OK
 
-    return {}
+    return JSONResponse(status_code=status.HTTP_200_OK, content={})
+
+
+@router.get("/game", responses={status.HTTP_200_OK: {"model": Game}})
+async def start_game():
+    """start a new game session endpoint
+
+    Args:
+        game (Game): Game Model
+
+        Returns:
+            response: Game Model
+    """
+    engine = Engine()
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=engine.game())
+
+
+@router.post("/game", responses={status.HTTP_200_OK: {"model": Game}})
+async def gaming(game: Game):
+    """load game session and proccess new round endpoint
+
+    Args:
+        game (Game): Game Model
+
+        Returns:
+            _type_: Game Model
+    """
+    engine = Engine()
+
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=engine.get_game_session(letter=game.letter, session=game.session),
+    )
